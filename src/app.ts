93c0c6135984e97121ad603ccfede3c9d32ebd6e
Darius Kulevicius <dariuux@gmail.com>
2021-09-21 21:40:18 +0300

    heroku correct port

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-const port: number = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/test', (_, res: Response) => {
   res.send('Hello, its airis');
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Connected to app ${port}`)
 })
