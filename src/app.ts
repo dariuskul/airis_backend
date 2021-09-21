39d754410254f673768162a6afaca87bb36b5cb1
Darius Kulevicius <dariuux@gmail.com>
2021-09-21 21:25:16 +0300

    heroku test

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ const app = express();
 
 const port: number = 5000;
 
-app.get('/', (_, res: Response) => {
+app.get('/test', (_, res: Response) => {
   res.send('Hello, its airis');
 });
 
 app.listen(port, () => {
-  console.log(`Connected to port ${port}`)
+  console.log(`Connected to app ${port}`)
 })
