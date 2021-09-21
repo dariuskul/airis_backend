4f6f800ca02313b5ede77c863143a5bef1d8fd58
Darius Kulevicius <dariuux@gmail.com>
2021-09-21 22:01:54 +0300

    done

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,10 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
 app.get('/test', (_, res: Response) => {
   res.send('Hello, its airis');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Connected to app ${port}`)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Connected to app`)
 })
