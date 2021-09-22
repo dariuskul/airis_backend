f5ee0e3f2d09aec9ad5b5cdb8a6b3e64b4784bc6
Darius Kulevicius <dariuux@gmail.com>
2021-09-22 20:46:21 +0300

    fixed issues

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/test', (_, res: Response) => {
-  res.send('Hello, its airis');
+app.get('/', (_, res: Response) => {
+  res.send('Hello, its airisss');
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Connected to app`)
+  console.log(`Connected to apps`)
 })
